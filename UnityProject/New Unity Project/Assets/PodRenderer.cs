9a9dca02df14b54658f530c0630cea442fc2ed64
ZevTheBunny <47234179+ZevTheBunny@users.noreply.github.com>
2023-02-03 17:54:16 -0800

    Updated H2O To autogenerate necelle positions

---
+++ UnityProject/New Unity Project/Assets/PodRenderer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodRenderer : MonoBehaviour
+{
+    [Tooltip("Transform for the cockpit")]
+    public Transform Cab;
+
+    public List<Necelle> Nacelles = new List<Necelle>(); 
+
+
+    private void OnDrawGizmos()
+    {
+        foreach (Necelle item in Nacelles)
+        {
+            item.Cab = Cab;
+            item.UpdatePosition();
+        }
+    }
+}
+
+
+[System.Serializable]
+public class Necelle
+{
+    public Transform Nacell;
+    public Vector3 Offset;
+    public LineRenderer Connector;
+    [HideInInspector] public Transform Cab;
+
+    public void UpdatePosition()
+    {
+        Nacell.localPosition = Cab.position + Offset;
+        
+        Connector.SetPositions(new Vector3[] { Nacell.position, Cab.position });
+    }
+}
