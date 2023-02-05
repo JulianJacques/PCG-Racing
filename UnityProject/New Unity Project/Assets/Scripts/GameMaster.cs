50e16cbc6c814e871efabe311837a790fa3d0e96
JJProgrammer72 <32990515+JJProgrammer72@users.noreply.github.com>
2023-02-05 14:30:50 -0800

    Update GameMaster.cs

--- UnityProject/New Unity Project/Assets/Scripts/GameMaster.cs
+++ UnityProject/New Unity Project/Assets/Scripts/GameMaster.cs
@@ -13,14 +13,10 @@ public class GameMaster : MonoBehaviour
     }
 
     private bool autoMode = false;
-    StreamWriter writer;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        string path = "Assets/Resources/timekeeper.csv";
-        //Write some text to the test.txt file
-        writer = new StreamWriter(path, true);
     }
 
     // Update is called once per frame
@@ -45,8 +41,6 @@ public class GameMaster : MonoBehaviour
 
     public void WriteTime(float time)
     {
-        writer.WriteLine(time);
-        writer.Close();
     }
     
     public void GameOver()
